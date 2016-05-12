eea819c4d7402a3599b551f3e4f9ae822233d5d4
Christian Fei <crifei93@gmail.com>
2016-05-12 14:42:26 +0200

    going nowhere

--- opening_hours/lib/opening_hours.ex
+++ opening_hours/lib/opening_hours.ex
@@ -1,2 +1,22 @@
 defmodule OpeningHours do
+  @work_days Application.get_env(:opening_hours, :work_days)
+  @from Application.get_env(:opening_hours, :from)
+  @to Application.get_env(:opening_hours, :to)
+
+  def is_open_on(date) do
+    # Regex.named_captures(~r/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})/g, date)
+    # |> values_to_integers
+    # |> date_time_captures_to_date_time
+  end
+
+  defp values_to_integers keyword_list do
+    keyword_list
+       |> Enum.map(fn {name, value} ->
+       {name, safe_to_integer(value)}
+     end)
+   end
+
+  defp date_time_captures_to_date_time captures do
+     {{captures[:year], captures[:month], captures[:day]}, {captures[:hour], captures[:minute], captures[:second]}}
+  end
 end
--- opening_hours/test/opening_hours_test.exs
+++ opening_hours/test/opening_hours_test.exs
@@ -2,7 +2,8 @@ defmodule OpeningHoursTest do
   use ExUnit.Case
   doctest OpeningHours
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "returns true if is open on day" do
+    monday = '2016-05-09T12:00:00.000Z'
+    assert OpeningHours.is_open_on(monday) == true
   end
 end
