9166dd6cb07bb5efb6ee3c2fb89873af2386c5d0
Christian Fei <crifei93@gmail.com>
2016-05-13 14:32:02 +0200

    trying to go somewhere, but i'm missing the point

--- opening_hours/lib/opening_hours.ex
+++ opening_hours/lib/opening_hours.ex
@@ -2,21 +2,16 @@ defmodule OpeningHours do
   @work_days Application.get_env(:opening_hours, :work_days)
   @from Application.get_env(:opening_hours, :from)
   @to Application.get_env(:opening_hours, :to)
+  @extract_date ~r/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T(?<hours>\d{2}):(?<minutes>\d{2}):(?<seconds>\d{2})\.(?<millis>\d{3})\w+$/
 
   def is_open_on(date) do
-    # Regex.named_captures(~r/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})/g, date)
-    # |> values_to_integers
-    # |> date_time_captures_to_date_time
+    parsed_date = Regex.named_captures(@extract_date, date)
+                  |> to_date_time
+
+    IO.inspect parsed_date
   end
 
-  defp values_to_integers keyword_list do
-    keyword_list
-       |> Enum.map(fn {name, value} ->
-       {name, safe_to_integer(value)}
-     end)
-   end
-
-  defp date_time_captures_to_date_time captures do
-     {{captures[:year], captures[:month], captures[:day]}, {captures[:hour], captures[:minute], captures[:second]}}
+  defp to_date_time(captures) do
+     {{captures["year"], captures["month"], captures["day"]}, {captures["hours"], captures["minutes"], captures["second"]}}
   end
 end
--- opening_hours/test/opening_hours_test.exs
+++ opening_hours/test/opening_hours_test.exs
@@ -3,7 +3,7 @@ defmodule OpeningHoursTest do
   doctest OpeningHours
 
   test "returns true if is open on day" do
-    monday = '2016-05-09T12:00:00.000Z'
+    monday = "2016-05-09T12:00:00.000Z"
     assert OpeningHours.is_open_on(monday) == true
   end
 end
