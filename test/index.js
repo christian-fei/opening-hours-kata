56dd43fd5457be6e4616c3441f32158c05ef59e0
Christian Fei <crifei93@gmail.com>
2017-01-21 17:53:56 +0100

    add first uat boilerplate

---
+++ test/index.js
@@ -0,0 +1,6 @@
+const {ok} = require('assert')
+const {test} = require('m.test')
+
+test('The opening days and hours of the shop need to be configurable, and can be scattered in the week (e.g. Mon, Wed, Fri from 08:00 to 16:00)', function () {
+  ok(true)
+})
